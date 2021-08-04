3ba3475181bcc8bf0652263712cafad34253213e
runnrobbot <36500288+runnrobbot@users.noreply.github.com>
2021-08-04 09:48:42 +0700

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,15 @@
+const config = require('../config.json')
+let prefix = config.prefix
+
+exports.run = async(client, message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(prefix)) {
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+
+        let commandfile = client.commands.get(cmd.slice(prefix.length));
+        if(!commandfile) return;
+        commandfile.run(client,message,args);
+    }
+}
