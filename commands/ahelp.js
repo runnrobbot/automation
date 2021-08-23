4fd6813343bc74a8fd55635f61233d3e3c848575
runnrobbot <36500288+runnrobbot@users.noreply.github.com>
2021-08-24 01:27:36 +0700

    Update ahelp.js

--- commands/ahelp.js
+++ commands/ahelp.js
@@ -12,22 +12,8 @@ exports.run = async (client, message, args ) => {
     .setFooter(`${copy}`)
     .setTimestamp()
     .setAuthor("PUBLIC COMMANDS")
-    .addField(".verify (username)", `Verify information user from roblox`)
-    .addField(".aboutgroup", `Information about Sriwijaya Group`)
     .addField(".info (username)", `Getting information user from roblox`)
     .addField(".suggestion (message)", `Suggest about group for further update `)
-    .addField(".databot", `About bot`)
     message.channel.send(send);
     
-    var embed = new Discord.MessageEmbed()
-    .setColor("GREEN")
-    .setFooter(`${copy}`)
-    .setTimestamp()
-    .setAuthor("HIGH RANK COMMANDS")
-    .addField(".shout (message)", `Give information about group to postgroup`)
-    .addField(".rank (username) (namerank)", `Give the user new rank`)
-    .addField(".logs", `Give information logs about the group`)
-    message.channel.send(embed);
-    
-
 }
